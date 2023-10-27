3b1b1e3cd7655fe67a23b7fca659b2376cbfa012
Jônatas Santos <jts.msantos@gmail.com>
2023-10-27 17:01:29 -0300

    fix: debug stapi webhook

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -7,12 +7,10 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 });
 
 export const POST = async (request: Request) => {
-  console.log("called")
   const signature = request.headers.get("stripe-signature");
 
   if (!signature) {
-    console.log("No signature sent")
-    return NextResponse.json({}, {status: 500, statusText: "No signature sent"});
+    return NextResponse.error();
   }
 
   const text = await request.text();
@@ -20,12 +18,10 @@ export const POST = async (request: Request) => {
   const event = stripe.webhooks.constructEvent(
     text,
     signature,
-    process.env.STRIPE_WEBHOOK_SECRET_KEY, 
+    process.env.STRIPE_WEBHOOK_SECRET_KEY,
   );
-  console.log(event.type)
 
   if (event.type === "checkout.session.completed") {
-    console.log("ENTROU", "checkout.session.completed")
     const session = event.data.object as any;
 
     const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
