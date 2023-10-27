1098e56a8ffb2c7c009140bb33d4d3218290db94
Jônatas Santos <jts.msantos@gmail.com>
2023-10-27 17:29:43 -0300

    fix: debug stapi webhook

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -1,47 +1,59 @@
+
 import { prismaClient } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
 });
 
-export const POST = async (request: Request) => {
-  const signature = request.headers.get("stripe-signature");
+export const POST = async (req: Request) => { 
+  try {
+    const body = await req.text();
+    const signature = headers().get("stripe-signature");
 
-  if (!signature) {
-    return NextResponse.error();
-  }
+    if (!signature) {
+      return NextResponse.error();
+    }
 
-  const text = await request.text();
-
-  const event = stripe.webhooks.constructEvent(
-    text,
-    signature,
-    process.env.STRIPE_WEBHOOK_SECRET_KEY,
-  );
-
-  if (event.type === "checkout.session.completed") {
-    const session = event.data.object as any;
-
-    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
-      event.data.object.id,
-      {
-        expand: ["line_items"],
-      },
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET_KEY,
     );
-    const lineItems = sessionWithLineItems.line_items;
 
-    // ATUALIZAR PEDIDO
-    await prismaClient.order.update({
-      where: {
-        id: session.metadata.orderId,
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object as any;
+
+      const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+        event.data.object.id,
+        {
+          expand: ["line_items"],
+        },
+      );
+      const lineItems = sessionWithLineItems.line_items;
+
+      // ATUALIZAR PEDIDO
+      await prismaClient.order.update({
+        where: {
+          id: session.metadata.orderId,
+        },
+        data: {
+          status: "PAYMENT_CONFIRMED",
+        },
+      });
+    }
+
+    return NextResponse.json({ received: true });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: "something went wrong",
+        ok: false,
       },
-      data: {
-        status: "PAYMENT_CONFIRMED",
-      },
-    });
+      { status: 500 },
+    );
   }
-
-  return NextResponse.json({ received: true });
 };
