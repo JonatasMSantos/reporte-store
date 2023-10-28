545d65f9bccd4261e5b970486e0face08aebdaa9
Jônatas Santos <jts.msantos@gmail.com>
2023-10-27 21:12:49 -0300

    fix: debug stapi webhook

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -1,55 +1,47 @@
 import { prismaClient } from "@/lib/prisma";
-import Stripe from "stripe";
-
 import { NextResponse } from "next/server";
-import { buffer } from "node:stream/consumers";
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
 });
 
-export async function POST(req: any) {
-  const rawBody = await buffer(req.body);
-  let event;
-  try {
-    event = stripe.webhooks.constructEvent(
-      rawBody,
-      req.headers.get("stripe-signature") as string,
-      process.env.STRIPE_WEBHOOK_SECRET_KEY as string,
-    );
+export const POST = async (request: Request) => {
+  const signature = request.headers.get("stripe-signature");
 
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object as any;
-
-      const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
-        event.data.object.id,
-        {
-          expand: ["line_items"],
-        },
-      );
-      const lineItems = sessionWithLineItems.line_items;
-
-      // ATUALIZAR PEDIDO
-      await prismaClient.order.update({
-        where: {
-          id: session.metadata.orderId,
-        },
-        data: {
-          status: "PAYMENT_CONFIRMED",
-        },
-      });
-    }
-
-    return NextResponse.json({ received: true });
-  } catch (err) {
-    console.log(err);
-    return NextResponse.json(
-      {
-        message: "Webhook signature verification failed",
-      },
-      {
-        status: 400,
-      },
-    );
+  if (!signature) {
+    return NextResponse.error();
   }
-}
+
+  const text = await request.text();
+
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  );
+
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as any;
+
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ["line_items"],
+      },
+    );
+    const lineItems = sessionWithLineItems.line_items;
+
+    // ATUALIZAR PEDIDO
+    await prismaClient.order.update({
+      where: {
+        id: session.metadata.orderId,
+      },
+      data: {
+        status: "PAYMENT_CONFIRMED",
+      },
+    });
+  }
+
+  return NextResponse.json({ received: true });
+};
